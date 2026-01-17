ecad6532e5f0175673d42839af1ff4d6aab1a85d
sqth92-beep <sq.th92@gmail.com>
2026-01-18 01:59:59 +0300

    Update queryClient.ts

--- client/src/lib/queryClient.ts
+++ client/src/lib/queryClient.ts
@@ -1,5 +1,5 @@
 import { QueryClient, QueryFunction } from "@tanstack/react-query";
-
+const BASE_URL = "https://delini-backend.onrender.com";
 async function throwIfResNotOk(res: Response) {
   if (!res.ok) {
     const text = (await res.text()) || res.statusText;
@@ -12,7 +12,7 @@ export async function apiRequest(
   url: string,
   data?: unknown | undefined,
 ): Promise<Response> {
-  const res = await fetch(url, {
+  const res = await fetch(`${BASE_URL}${url}`, {
     method,
     headers: data ? { "Content-Type": "application/json" } : {},
     body: data ? JSON.stringify(data) : undefined,
@@ -29,8 +29,7 @@ export const getQueryFn: <T>(options: {
 }) => QueryFunction<T> =
   ({ on401: unauthorizedBehavior }) =>
   async ({ queryKey }) => {
-    const res = await fetch(queryKey.join("/") as string, {
-      credentials: "include",
+const res = await fetch(`${BASE_URL}/api/${queryKey.join("/")}`, {      credentials: "include",
     });
 
     if (unauthorizedBehavior === "returnNull" && res.status === 401) {
