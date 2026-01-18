e4d8b272502d7cee86fdb5ef5549ba957433019d
sqth92-beep <sq.th92@gmail.com>
2026-01-18 04:07:06 +0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,21 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.delini.app',
-  appName: 'Delini',
-  webDir: 'dist/public'
+  appName: 'delini-app',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https',
+    cleartext: true,
+    allowNavigation: [
+      'delini-backend.onrender.com'
+    ]
+  },
+  plugins: {
+    CapacitorHttp: {
+      enabled: true
+    }
+  }
 };
 
 export default config;
