21ac5d566284af6b99589fabe35f66e133a68ef1
sqth92-beep <sq.th92@gmail.com>
2026-01-18 04:17:34 +0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.delini.app',
   appName: 'delini-app',
-  webDir: 'dist', // تأكد أن هذا المجلد هو الذي ينتج عن عملية الـ build
+  webDir: 'dist',
   server: {
     androidScheme: 'https',
     allowNavigation: [
